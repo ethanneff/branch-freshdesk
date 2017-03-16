9e533d6738cfff3fbd5584ba394a3174259935c1
Ethan Neff <ethan.neff@eneff.com>
2017-03-16 15:21:51 -0700

    tryign to get server to work

--- src/app.js
+++ src/app.js
@@ -1,34 +1,20 @@
-// lib
-var express = require('express')
+var express = require('express');
+var app = express();
 
-// app
-var app = express()
-
-app.get('/', function (req, res) {
-  res.send('Hello World!')
-})
-
-app.listen(3000, function () {
-  console.log('Example app listening on port 3000!')
-})
-
-// var express = require('express');
-// var app = express();
-
-// app.set('port', (process.env.PORT || 5000));
+app.set('port', (process.env.PORT || 5000));
 
 // app.use(express.static(__dirname + '/public'));
 
 // // views is directory for all template files
 // app.set('views', __dirname + '/views');
 
-// app.get('/', function(request, response) {
-//   response.send('Hello World!')
-// });
+app.get('/', function(request, response) {
+  response.send('Hello World!')
+});
 
-// app.listen(app.get('port'), function() {
-//   console.log('Node app is running on port', app.get('port'));
-// });
+app.listen(app.get('port'), function() {
+  console.log('Node app is running on port', app.get('port'));
+});
 
 
 // var express = require('express');
