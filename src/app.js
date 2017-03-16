26c48956a636ac8efa44dc0f8eb767e0fd4d0955
Ethan Neff <ethan.neff@eneff.com>
2017-03-16 15:17:20 -0700

    trying to get web to show

--- src/app.js
+++ src/app.js
@@ -1,6 +1,6 @@
 // npm run start
 
-// lib
+lib
 var express = require('express')
 
 // app
@@ -13,3 +13,43 @@ app.get('/', function (req, res) {
 app.listen(3000, function () {
   console.log('Example app listening on port 3000!')
 })
+
+// var express = require('express');
+// var app = express();
+
+// app.set('port', (process.env.PORT || 5000));
+
+// app.use(express.static(__dirname + '/public'));
+
+// // views is directory for all template files
+// app.set('views', __dirname + '/views');
+
+// app.get('/', function(request, response) {
+//   response.send('Hello World!')
+// });
+
+// app.listen(app.get('port'), function() {
+//   console.log('Node app is running on port', app.get('port'));
+// });
+
+
+// var express = require('express');
+// var app = express();
+
+// app.set('port', (process.env.PORT || 5000));
+
+// app.use(express.static(__dirname + '/public'));
+
+// // views is directory for all template files
+// app.set('views', __dirname + '/views');
+// app.set('view engine', 'ejs');
+
+// app.get('/', function(request, response) {
+//   response.render('pages/index');
+// });
+
+// app.listen(app.get('port'), function() {
+//   console.log('Node app is running on port', app.get('port'));
+// });
+
+
