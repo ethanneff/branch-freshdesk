e8222183affc5293696d61cdcd1469e07bd30a00
Ethan Neff <ethan.neff@eneff.com>
2017-03-29 17:18:51 -0700

    update

--- src/server/app.js
+++ src/server/app.js
@@ -21,7 +21,6 @@ function run () {
 
   // server
   app.use(helmet())
-  app.use(express.static(path.join(__dirname, './../../bower_components')))
   app.use(express.static(path.join(__dirname, './../client')))
   app.get('/', function (request, response) {
     response.sendFile(path.join(__dirname, '/index.html'))
